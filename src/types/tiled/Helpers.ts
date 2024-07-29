2d30981fa2e046257fb5640dce64b6c5c2c42be2
Gabriel-ISS <gabriel69iss@gmail.com>
2024-07-29 16:09:52 -0400

    tipos para facilitar el desarrollo

---
+++ src/types/tiled/Helpers.ts
@@ -0,0 +1,6 @@
+export type Tiles = number
+
+export type Coordinates = [x: number, y: number]
+export type TileCoordinates = [x: Tiles, y: Tiles]
+
+export type TileDimensions = [width: number, height: number]
