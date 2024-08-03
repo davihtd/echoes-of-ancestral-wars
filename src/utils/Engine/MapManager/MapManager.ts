a44a0693b19d8e3848940c67fc4840d8e91a17cb
Gabriel-ISS <gabriel69iss@gmail.com>
2024-08-03 14:09:08 -0400

    mover cámara

--- src/utils/Engine/MapManager/MapManager.ts
+++ src/utils/Engine/MapManager/MapManager.ts
@@ -1,5 +1,8 @@
 import Element from '../../Element';
+import Camera from './Camera';
+import Position from '../Position';
 import CanvasLayer from './CanvasLayer';
+import type Map from './Map';
 import MapDataLoader from './MapDataLoader';
 
 
@@ -11,14 +14,7 @@ type CanvasLayers = {
 export default class MapManager extends Element<'div'> {
   private layers: CanvasLayers | null = null
   readonly data: MapDataLoader;
-
-  get zoom() {
-    return Number(this.element.style.scale)
-  }
-
-  set zoom(value: number) {
-    this.element.style.scale = value.toString()
-  }
+  readonly camera: Camera;
 
   constructor(mapContainerID: string, mapDataPath: string) {
     const $mapContainer = Element.get('#' + mapContainerID)
@@ -27,30 +23,42 @@ export default class MapManager extends Element<'div'> {
     this.element.style.position = 'fixed'
     this.zoom = 2.5
 
-    this.data = new MapDataLoader(mapDataPath, map => {
+    this.data = new MapDataLoader(mapDataPath, map => this.#loadMap(map))
 
-      this.layers = {
-        background: new CanvasLayer(this.element, 0, map),
-        foreground: new CanvasLayer(this.element, 2, map),
+    const mapPosition = new Position(this.element)
+    this.camera = new Camera(mapPosition)
+  }
+
+  get zoom() {
+    return Number(this.element.style.scale)
+  }
+
+  set zoom(value: number) {
+    this.element.style.scale = value.toString()
+    this.camera.zoom = value
+  }
+
+  #loadMap(map: Map) {
+    this.layers = {
+      background: new CanvasLayer(this.element, 0, map),
+      foreground: new CanvasLayer(this.element, 2, map),
+    }
+
+    map.data.layers.forEach(layer => {
+      console.log(layer.name)
+      switch (layer.name) {
+        case 'background':
+          this.layers?.background.drawTileLayerGroup(layer)
+          break;
+        case 'foreground':
+          this.layers?.foreground.drawTileLayerGroup(layer)
+          break;
+        case 'GameObjects':
+          // agregar a un ObjectLayer y guardar coordenadas en tiles?
+          break;
+        default:
+          console.warn(`Layer "${layer.name}" is not recognized`)
       }
-
-      map.data.layers.forEach(layer => {
-        console.log(layer.name)
-        switch (layer.name) {
-          case 'background':
-            this.layers?.background.drawTileLayerGroup(layer)
-            break;
-          case 'foreground':
-            this.layers?.foreground.drawTileLayerGroup(layer)
-            break;
-          case 'GameObjects':
-            // agregar a un ObjectLayer y guardar coordenadas en tiles?
-            break;
-          default:
-            console.warn(`Layer "${layer.name}" is not recognized`)
-        }
-      })
     })
   }
 }
-
