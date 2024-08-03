123bf22774f6187a8855ac8df98ef74c19ac4ea7
Gabriel-ISS <gabriel69iss@gmail.com>
2024-08-03 09:51:02 -0400

    Actualización de FPSController, ahora con delta

--- src/utils/Engine/FPSController.ts
+++ src/utils/Engine/FPSController.ts
@@ -1,49 +1,71 @@
 type Callback = () => void
 
-/** 
- * - Si tiene _ es de uso interno
- * - El resto es de solo lectura
- */
-const FPSController = {
-  _executionID: null as null | number,
-  _lastRegisteredTime: 0,
-  currentFrame: 0,
-  FPS: 0,
-  events: [] as Callback[],
-  startGame(currentTime: DOMHighResTimeStamp = 0) {
-    const updateFPS = () => {
-      FPSController.currentFrame++;
 
-      const oneSecond = 999
-      const millisecondsFromLastExecution = currentTime - FPSController._lastRegisteredTime
-      if (millisecondsFromLastExecution > oneSecond) {
-        FPSController._lastRegisteredTime = currentTime
-        FPSController.FPS = FPSController.currentFrame
-        FPSController.currentFrame = 0
-      }
-    }
+export default class FPSController {
+  private static executionID: null | number = null;
+  private static lastRegisteredTimeAfterSecond = 0;
+  private static lastRegisteredTime = 0;
+  private static events: Callback[] = [];
+  
+  private static _currentFrame = 0;
+  /** @deprecated Ya no hay razón para que sea publico. Solo hace falta eliminar getDebouncedFn */
+  public static get currentFrame() {
+    return FPSController._currentFrame;
+  }
+  private static _FPS = 0;
+  /** FramesPerSecond - FotogramasPorSegundo */
+  public static get FPS() {
+    return FPSController._FPS;
+  }
 
-    FPSController._executionID = requestAnimationFrame(FPSController.startGame)
-    updateFPS()
+  private static _delta: number = 0;
+  /** Delta se refiere a una diferencia de dos valores. En este caso es la diferencia de tiempo entre
+   * dos fotogramas en segundos. Para que el movimiento de cualquier objeto del juego sea suave 
+   * independientemente de la tasa de fotogramas puedes multiplicar delta por el valor de movimiento.
+   * @example 
+   *  Player.moveRight(200 * FPSController.delta)
+   * // Player se moverá a 200 unidades por segundo
+   */
+  public static get delta(): number {
+    return FPSController._delta;
+  }
 
+  static startGame(currentTime: DOMHighResTimeStamp = 0) {
+    FPSController._delta = (currentTime - FPSController.lastRegisteredTime) / 1000
+    FPSController.lastRegisteredTime = currentTime
+    FPSController.executionID = requestAnimationFrame(FPSController.startGame)
+    FPSController.updateFPS(currentTime)
     FPSController.events.forEach(event => event())
-  },
-  stopGame() {
-    if (typeof FPSController._executionID != 'number') return;
-    cancelAnimationFrame(FPSController._executionID)
-    FPSController._executionID = null
-    FPSController._lastRegisteredTime = 0
-    FPSController.FPS = 0
-    FPSController.currentFrame = 0
-  },
-  addEvent(event: Callback) {
+  }
+
+  private static updateFPS = (currentTime: number) => {
+    FPSController._currentFrame++;
+
+    const oneSecond = 999
+    const millisecondsFromLastExecution = currentTime - FPSController.lastRegisteredTimeAfterSecond
+    if (millisecondsFromLastExecution > oneSecond) {
+      FPSController.lastRegisteredTimeAfterSecond = currentTime
+      FPSController._FPS = FPSController.currentFrame
+      FPSController._currentFrame = 0
+    }
+  }
+
+  static stopGame() {
+    if (typeof this.executionID != 'number') return;
+    cancelAnimationFrame(this.executionID)
+    this.executionID = null
+    this.lastRegisteredTimeAfterSecond = 0
+    this._FPS = 0
+    this._currentFrame = 0
+  }
+
+  static addEvent(event: Callback) {
     this.events.push(event)
   }
 }
 
-export default FPSController
 
-/**Retorna la misma función pero modificada para que solo se ejecute una vez por fotograma */
+/** @deprecated Mejor usa FPSController.delta */
 export function getDebouncedFn(fn: Callback): Callback {
   let prevFrame: null | number = null;
   return () => {
